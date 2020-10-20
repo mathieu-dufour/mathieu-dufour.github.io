e73e7dd6b5e5f1b3be79d00ac58a41744431d660
Mathieu Dufour <mathieu_dufour@hotmail.com>
2020-10-20 01:41:26 -0400

    feat: Add fullpage plugin for smooth scrolling animation between sections

--- app.js
+++ app.js
@@ -7,3 +7,15 @@ tl.to('.slider', { y: "-100%", duration: 1.5, delay: 0.8 });
 tl.to('.intro', { y: "-100%", duration: 1 }, "-=1.1"); // Starts 1.1 second earlier; same time as slider animation
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "-=0.55");
 tl.fromTo(".page-title-container", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "-=1.5");
+
+
+// Call fullPage.js
+new fullpage('#fullpage', {
+	//options
+	autoScrolling:true,
+	anchors:["home", "education", "skills", "experience", "more", "contact"],
+	licenseKey: '40319E3A-196B4662-8916DF4C-3753A081',
+});
+
+//methods
+//fullpage_api.setAllowScrolling(false);
