2b306e05beab73ea47a2b2a65e2eaf6fe557b80c
Mathieu Dufour <mathieu_dufour@hotmail.com>
2021-01-14 02:21:05 -0500

    First version of mobile compatibility media query
    - Works on Pixel 2XL portrait mode (411x823)

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 new fullpage('#fullpage', {
 	//options
 	autoScrolling: true,
+	responsiveWidth: 824,
 	anchors: ["home", "education", "skills", "experience", "more", "contact"],
 	licenseKey: '40319E3A-196B4662-8916DF4C-3753A081',
 	// responsiveWidth: 600,
@@ -25,12 +26,11 @@ var currentYear = new Date().getFullYear();
 document.getElementById('copyright').innerHTML = "&copy; Copyright " + currentYear + ", Mathieu Dufour";
 
 function flipCard(cardName, side, axis) {
-	if (side == 'front') rotateCmd = "rotate" + axis + "(0deg)";
-	else rotateCmd = "rotate" + axis + "(180deg)";
-
 	var cardID = cardName + '-flip-card';
-
-	document.getElementById(cardID).style.transform = rotateCmd;
+	var element = document.getElementById(cardID)
+	console.log(cardID);
+	if (side == 'back') element.classList.add('flipped-card-' + axis)
+	else element.classList.remove('flipped-card-' + axis)
 }
 
 // Remove default outline if user is not using tab (accessibility)
